dc55c60ec326525cabceb2230b37acdb5f67fe7a
DavidAFrederick <davidafrederick.va.usa.earth@gmail.com>
2025-10-17 21:54:29 -0400

    Cleaning up code

--- obstacle_avoidance_Example/controllers/avoid_obstacles/avoid_obstacles.py
+++ obstacle_avoidance_Example/controllers/avoid_obstacles/avoid_obstacles.py
@@ -6,8 +6,6 @@
 from controller import Robot, Motor, PositionSensor
 
 #===============================================================================================
-
-#===============================================================================================
 class ThymioRobot(Robot):
 
     def __init__(self):     # Initialize the robot
@@ -17,14 +15,14 @@ class ThymioRobot(Robot):
 
         # Get simulation step duration.
         self.timeStep = int(self.robot.getBasicTimeStep())   # Results in 16 milliseconds
-        # self.timeStep = 20  # milliSeconds
-        print (f"self.timeStep {self.timeStep}")
 
-        # - - ( Motors ) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # - - ( Constants ) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         # Constants of the Thymio II motors and distance sensors.
         self.maxMotorVelocity = 10
         self.distanceSensorCalibrationConstant = 360
+
+        # - - ( Instantiate the Motors ) - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         # Get left and right wheel motors.
         self.leftMotor  = self.robot.getDevice("motor.left")
@@ -35,7 +33,7 @@ class ThymioRobot(Robot):
         self.rightMotor.setPosition(float('inf'))
 
 
-        # - - ( Range Sensors ) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # - - ( Instantate the Range Sensors ) - - - - - - - - - - - - - - - - - - - - - - - - 
 
         # Get frontal distance sensors.
         self.outerLeftSensor    = self.robot.getDevice("prox.horizontal.0")
@@ -51,18 +49,19 @@ class ThymioRobot(Robot):
         self.centralRightSensor.enable(self.timeStep)
         self.outerRightSensor.enable(self.timeStep)
 
-        # - - ( LEDs ) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # - - ( Instantiate the LEDs ) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
         self.top_RGB_led = self.robot.getDevice('leds.top') # Access the  RGB LEDs
         self.bottom_left_RGB_led = self.robot.getDevice('leds.bottom.left') 
         self.bottom_right_RGB_led = self.robot.getDevice('leds.bottom.right') 
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - [Low Level Methods ]- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
-    def move_robot_time_forward(self):
+    def move_robot_time_forward(self) -> None:
         self.robot.step(self.timeStep)
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     def get_simulation_is_not_complete(self) -> bool:
         # (-1 is returned when Webots terminates the controller. Other values are the actual time step in millisec)
         if (self.robot.step(self.timeStep) != -1):   #Not complete
@@ -72,23 +71,17 @@ class ThymioRobot(Robot):
         return is_not_complete
     
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    def print_sensor_values(self):
-        # self.outerLeftSensorValue    = self.outerLeftSensor.getValue()    / self.distanceSensorCalibrationConstant
-        # self.centralLeftSensorValue  = self.centralLeftSensor.getValue()  / self.distanceSensorCalibrationConstant
-        # self.centralSensorValue      = self.centralSensor.getValue()      / self.distanceSensorCalibrationConstant
-        # self.centralRightSensorValue = self.centralRightSensor.getValue() / self.distanceSensorCalibrationConstant
-        # self.outerRightSensorValue   = self.outerRightSensor.getValue()   / self.distanceSensorCalibrationConstant
-
+    def print_sensor_values(self) -> None:
         self.outerLeftSensorValue    = self.outerLeftSensor.getValue()      # Touching is 2500
         self.centralLeftSensorValue  = self.centralLeftSensor.getValue()  
         self.centralSensorValue      = self.centralSensor.getValue()      
         self.centralRightSensorValue = self.centralRightSensor.getValue() 
         self.outerRightSensorValue   = self.outerRightSensor.getValue()   
 
-        # print (f"Sensors {self.outerLeftSensorValue:5.2f} {self.centralLeftSensorValue:5.2f} {self.centralSensorValue:5.2f} {self.centralRightSensorValue:5.2f} {self.outerRightSensorValue:5.2f}")
+        print (f"Sensors {self.outerLeftSensorValue:5.2f} {self.centralLeftSensorValue:5.2f} {self.centralSensorValue:5.2f} {self.centralRightSensorValue:5.2f} {self.outerRightSensorValue:5.2f}")
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    def set_both_wheel_speeds(self, percent_velocity):
+    def set_both_wheel_speeds(self, percent_velocity : float) -> None:
         # Set wheel velocities based on sensor values, prefer right turns if the central sensor is triggered.
         # self.left_velocity = self.initialVelocity - (self.centralRightSensorValue + self.outerRightSensorValue) 
         # self.right_velicity = self.initialVelocity - (self.centralLeftSensorValue + self.outerLeftSensorValue)  - self.centralSensorValue
@@ -97,22 +90,19 @@ class ThymioRobot(Robot):
         self.rightMotor.setVelocity(speed)
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    def set_wheel_speeds_separately(self, left_percent_velocity, right_percent_velocity):
-        # Set wheel velocities based on sensor values, prefer right turns if the central sensor is triggered.
-        # self.left_velocity = self.initialVelocity - (self.centralRightSensorValue + self.outerRightSensorValue) 
-        # self.right_velicity = self.initialVelocity - (self.centralLeftSensorValue + self.outerLeftSensorValue)  - self.centralSensorValue
+    def set_wheel_speeds_separately(self, left_percent_velocity : float, right_percent_velocity : float) -> None:
         left_speed = left_percent_velocity * self.maxMotorVelocity
         right_speed = right_percent_velocity * self.maxMotorVelocity
         self.leftMotor.setVelocity(left_speed)
         self.rightMotor.setVelocity(right_speed)
 
-
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    def move_forward_at_percent_speed_x_for_y_seconds(self, percent_speed, time_seconds):
+    # - - [ High Level Methods ]- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    def move_forward_at_percent_speed_x_for_y_seconds(self, percent_speed : float, time_seconds : float) -> None:
         print(f"Moving forward at {percent_speed:4.1f} percent for {time_seconds:4.1f} seconds.  Current time {self.robot.getTime():5.1f}")
         self.set_RGB_LED_1()
-        # self.set_both_wheel_speeds(percent_speed)
-        self.set_wheel_speeds_separately(percent_speed,  percent_speed * 0.3)
+        self.set_both_wheel_speeds(percent_speed)
         start_time = self.robot.getTime()
         time_in_loop = 0
         # print (f"time_in_loop: {(time_in_loop*1000):5.2f}      time_seconds:  {time_seconds}")
@@ -125,17 +115,19 @@ class ThymioRobot(Robot):
         self.set_both_wheel_speeds(0)
         print (f"Stopping movement at: {current_time:5.1f}")
 
-    def move_using_position_sensor(self):
-        distance = 10.1
-                # Disable motor PID control mode.
-        self.leftMotor.setPosition(distance)         # Set the "Stop Position" to infinity
-        self.rightMotor.setPosition(distance)
-        
-        print(f"Position:  {self.leftMotor.getPosition()}")
+    def move_using_position_sensor(self, distance_to_travel : float, speed_to_travel : float) -> None:
+        self.leftMotor.setPosition(float('inf'))         # Set the "Stop Position" to infinity
+        self.rightMotor.setPosition(float('inf'))   
+        self.set_both_wheel_speeds(speed_to_travel)
+        self.current_position = self.leftMotor.getPosition()
+        while (self.current_position < distance_to_travel):
+            self.move_robot_time_forward()
+            self.current_position = self.leftMotor.getPosition()
+            print(f"Current Position:  {self.leftMotor.getPosition()}      Target Position:  {distance_to_travel}")
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     def stop_and_pause_for_x_seconds(self, time_seconds):
-        # print(f"Stopping for {time_seconds:4.1f} seconds.  Current time {self.robot.getTime():5.1f}")
+        print(f"Stopping for {time_seconds:4.1f} seconds.  Current time {self.robot.getTime():5.1f}")
         self.set_RGB_LED_2()
         self.set_both_wheel_speeds(0)
         start_time = self.robot.getTime()
@@ -145,11 +137,10 @@ class ThymioRobot(Robot):
             current_time = self.robot.getTime()
             time_in_loop = current_time - start_time
         self.set_both_wheel_speeds(0)
-        # print (f"Completed at: {current_time:5.1f}    {self.robot.getTime():5.1f}")
+        print (f"Completed at: {current_time:5.1f}    {self.robot.getTime():5.1f}")
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     def set_RGB_LED_1(self):    # (0xFF0000)
-        # self.top_RGB_led.set(color)
         self.top_RGB_led.set(0xFF0000)
         self.bottom_left_RGB_led.set(0x0000FF)
         self.bottom_right_RGB_led.set(0x000000)
